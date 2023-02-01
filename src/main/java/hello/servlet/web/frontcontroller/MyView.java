dedd355dadc880b116a1db14863052294a5c5245
JinYeongKim <handayoung11@gmail.com>
2023-02-01 10:26:22 +0900

    MyView에 model을 처리할 수 있도록 render method 오버로딩

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -17,5 +18,14 @@ public class MyView {
     public void render(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
         dispatcher.forward(req, resp);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        modelToRequestAttribute(model, req);
+        render(req, resp);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest req) {
+        model.forEach((key, value) -> req.setAttribute(key, value));
     }
 }
